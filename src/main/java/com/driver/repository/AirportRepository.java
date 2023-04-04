3fb7a8e1b9f97650db0979c7c006e1c4a7da2ee9
Pratik Bhagwat <pratikbhagwatdev@gmail.com>
2023-04-04 15:19:22 +0530

    fixed all null pointers

--- src/main/java/com/driver/repository/AirportRepository.java
+++ src/main/java/com/driver/repository/AirportRepository.java
@@ -14,8 +14,8 @@ public class AirportRepository {
 
     HashMap<String,Airport> airportDB = new HashMap<>();
     HashMap<Integer,Passenger> passengerDb = new HashMap<>();
-    HashMap<Flight,List<Passenger>> flightPassengerDb = new HashMap<>();
-    List<Flight> flightList = new ArrayList<>();
+    HashMap<Integer,List<Integer>> flightPassengerDb = new HashMap<>();
+    HashMap<Integer,Flight> flightDb = new HashMap<>();
 
 
     public void addAirport(Airport airport) {
@@ -47,7 +47,7 @@ public class AirportRepository {
         //Find the duration by finding the shortest flight that connects these 2 cities directly
         //If there is no direct flight between 2 cities return -1.
         double shortestDuration = Integer.MAX_VALUE * 1.00;
-        for (Flight f : flightList) {
+        for (Flight f : flightDb.values()) {
             if(f.getToCity().equals(toCity) && f.getFromCity().equals(fromCity)) {
                 shortestDuration = Math.min(shortestDuration, f.getDuration());
             }
@@ -66,10 +66,10 @@ public class AirportRepository {
         }
         City city = airport.getCity();
         int cnt = 0;
-        for (Flight flight : flightPassengerDb.keySet()) {
+        for (Flight flight : flightDb.values()) {
             if((flight.getFromCity().equals(city)||flight.getToCity().equals(city)) && flight.getFlightDate().equals(date)) {
-                List<Passenger> passengerList = flightPassengerDb.get(flight);
-                cnt += passengerList.size();
+                int flightID = flight.getFlightId();
+                cnt += flightPassengerDb.get(flightID).size();
             }
         }
         return cnt;
@@ -80,14 +80,7 @@ public class AirportRepository {
         //Price for any flight will be : 3000 + noOfPeopleWhoHaveAlreadyBooked*50
         //Suppose if 2 people have booked the flight already : the price of flight for the third person will be 3000 + 2*50 = 3100
         //This will not include the current person who is trying to book, he might also be just checking price
-        int noOfPeopleWhoHaveAlreadyBooked = 0;
-        for(Flight flight : flightPassengerDb.keySet()) {
-           if (flight.getFlightId() == flightId) {
-               List<Passenger> passengerList = flightPassengerDb.get(flight);
-               noOfPeopleWhoHaveAlreadyBooked = passengerList.size();
-               break;
-           }
-        }
+        int noOfPeopleWhoHaveAlreadyBooked = flightPassengerDb.get(flightId).size();
         return 3000 + noOfPeopleWhoHaveAlreadyBooked * 50;
     }
 
@@ -96,31 +89,27 @@ public class AirportRepository {
         //return a String "FAILURE"
         //Also if the passenger has already booked a flight then also return "FAILURE".
         //else if you are able to book a ticket then return "SUCCESS"
-        int flightFare = calculateFlightFare(flightId);
-
-
-        for (Flight flight : flightList) {
-            if(flight.getFlightId() == flightId) {
-                Passenger passenger = new Passenger();
-                passenger.setPassengerId(passengerId);
-
-                List<Passenger> passengerList = flightPassengerDb.get(flight);
-
-                if(passengerList.contains(passenger)) {
-                    return "FAILURE";
-                }
-
-                if(passengerList.size() < flight.getMaxCapacity()) {
-                    passengerList.add(passenger);
-                    flightPassengerDb.put(flight,passengerList);
-                    return "SUCCESS";
-                }
-                else if(passengerList.size() >= flight.getMaxCapacity()) {
-                    return "FAILURE";
-                }
+        if(Objects.nonNull(flightPassengerDb.get(flightId)) &&(flightPassengerDb.get(flightId).size()<flightDb.get(flightId).getMaxCapacity())) {
+            List<Integer> passengerList = flightPassengerDb.get(flightId);
+            if(passengerList.contains(passengerId)) {
+                return "FAILURE";
             }
+            passengerList.add(passengerId);
+            flightPassengerDb.put(flightId,passengerList);
+            return "SUCCESS";
         }
-        return null;
+        else if(Objects.isNull(flightPassengerDb.get(flightId))) {
+            flightPassengerDb.put(flightId,new ArrayList<>());
+            List<Integer> passengerList = flightPassengerDb.get(flightId);
+
+            if(passengerList.contains(passengerId)) {
+                return "FAILURE";
+            }
+            passengerList.add(passengerId);
+            flightPassengerDb.put(flightId,passengerList);
+            return "SUCCESS";
+        }
+        return "FAILURE";
     }
 
     public String cancelATicket(Integer flightId, Integer passengerId) {
@@ -129,47 +118,33 @@ public class AirportRepository {
         // Otherwise return a "SUCCESS" message
         // and also cancel the ticket that passenger had booked earlier on the given flightId
 
-        Passenger passenger = null;
-        for(Integer id : passengerDb.keySet()) {
-            if(id.equals(passengerId)) {
-                passenger = passengerDb.get(id);
-                break;
-            }
+        List<Integer> passengerList = flightPassengerDb.get(flightId);
+        if(passengerList == null) {
+            return "FAILURE";
         }
-
-        for(Flight flight : flightPassengerDb.keySet()) {
-            if(flight.getFlightId() == flightId) {
-                List<Passenger> passengerList = flightPassengerDb.get(flight);
-                if(passengerList == null) {
-                    return "FAILURE";
-                }
-                if(passengerList.contains(passenger)) {
-                    passengerList.remove(passenger);
-                    return "SUCCESS";
-                }
-            }
+        if(passengerList.contains(passengerId)) {
+            passengerList.remove(passengerId);
+            return "SUCCESS";
         }
         return "FAILURE";
     }
 
     public int countOfBookingsDoneByPassengerAllCombined(Integer passengerId) {
         //Tell the count of flight bookings done by a passenger: This will tell the total count of flight bookings done by a passenger :
-        Passenger passenger = null;
-        for(Integer id : passengerDb.keySet()) {
-            if(id.equals(passengerId)) {
-                passenger = passengerDb.get(id);
-                break;
-            }
-        }
         int cnt = 0;
-        for (List <Passenger> p : flightPassengerDb.values()) {
-            if(p.contains(passenger)) cnt++;
+        HashMap<Integer,List<Integer>> passengerFlightDb = new HashMap<>();
+
+        for(Map.Entry<Integer,List<Integer>> entry: flightPassengerDb.entrySet()) {
+            List<Integer> passengerList = entry.getValue();
+            for(Integer p: passengerList) {
+                if(p.equals(passengerId)) cnt++;
+            }
         }
         return cnt;
     }
 
     public String addFlight(Flight flight) {
-        flightList.add(flight);
+        flightDb.put(flight.getFlightId(),flight);
         return "SUCCESS";
         //Return a "SUCCESS" message string after adding a flight.
     }
@@ -177,9 +152,12 @@ public class AirportRepository {
     public String getAirportNameFromFlightId(Integer flightId) {
         //We need to get the starting airportName from where the flight will be taking off (Hint think of City variable if that can be of some use)
         //return null incase the flightId is invalid or you are not able to find the airportName
-        for (Flight f: flightList) {
-            if(f.getFlightId() == flightId) {
-                return f.getFromCity().name();
+        if (flightDb.containsKey(flightId)) {
+            City city = flightDb.get(flightId).getFromCity();
+            for (Airport a : airportDB.values()) {
+                if (a.getCity().equals(city)) {
+                    return a.getAirportName();
+                }
             }
         }
         return null;
@@ -188,20 +166,11 @@ public class AirportRepository {
     public int calculateRevenueOfAFlight(Integer flightId) {
         //Calculate the total revenue that a flight could have
         //That is of all the passengers that have booked a flight till now and then calculate the revenue
-        //Revenue will also decrease if some passenger cancels the flight
-        List<Passenger> passengers = null;
-        for (Flight f : flightPassengerDb.keySet()) {
-            if(f.getFlightId() == flightId) {
-                passengers = flightPassengerDb.get(f);
-                break;
-            }
-        }
-        int revenue = 0;
-        int size = passengers != null ? passengers.size() : 0;
-        for(int i = 0; i < size; i++) {
-            revenue += 3000 + (i * 50);
-        }
-        return revenue;
+        // will also decrease if some passenger cancels the flight
+        int noOfPeopleBookedFlight = flightPassengerDb.get(flightId).size();
+        int revenue = (noOfPeopleBookedFlight *(noOfPeopleBookedFlight + 1)) * 50;
+        int totalRevenue = 3000 * noOfPeopleBookedFlight + revenue;
+        return totalRevenue;
     }
 
     public String addPassenger(Passenger passenger) {
